5fd14a32c8cb9531c61da5a9264ee114488a9fd9
Melmoh19 <andersonhart003@gmail.com>
2025-06-12 20:21:25 +0300

    api.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,25 +2,33 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('Index page', () => {
-  const url = 'http://localhost:7865/';
+  const baseUrl = 'http://localhost:7865';
 
-  it('Correct status code?', (done) => {
-    request.get(url, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
+  it('should return correct status code', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
-  it('Correct result?', (done) => {
-    request.get(url, (err, res, body) => {
+  it('should return correct result', (done) => {
+    request.get(baseUrl, (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
 
-  it('Other? (Content-Type header)', (done) => {
-    request.get(url, (err, res, body) => {
-      expect(res.headers['content-type']).to.include('text/html');
+  it('should return correct content type', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(response.headers['content-type']).to.include('text/html');
+      done();
+    });
+  });
+
+  it('should handle GET request method', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(error).to.be.null;
+      expect(response).to.not.be.undefined;
       done();
     });
   });
