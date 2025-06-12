6b77787863717ecac0047419a4e04c3d4805fdde
Melmoh19 <andersonhart003@gmail.com>
2025-06-12 14:24:58 +0300

    0-calcul.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,7 +1,7 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
-const Utils = require('utils.js');
-const sendPaymentRequestToApi = require('3-payment.js');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('should call Utils.calculateNumber with SUM, 100, 20', () => {
