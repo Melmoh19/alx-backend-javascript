0202b2b452a17f590815c5cd55c2e048c61f23e1
Melmoh19 <andersonhart003@gmail.com>
2025-06-13 12:58:08 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,17 @@
-#!/usr/bin/env node
-
 // Display welcome message
-process.stdout.write('Welcome to Holberton, what is your name?\n');
+console.log('Welcome to ALX, what is your name?');
 
-// Set stdin encoding to utf8
+// Set stdin to resume and set encoding
+process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 // Listen for data input
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  console.log(`Your name is: ${name}`);
 });
 
 // Listen for end of input (when user ends the program)
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
