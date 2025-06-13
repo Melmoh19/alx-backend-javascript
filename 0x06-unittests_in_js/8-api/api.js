10f08002b2238048f4f2baa8a31c974d79449a41
Melmoh19 <andersonhart003@gmail.com>
2025-06-13 11:20:31 +0300

    api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,17 @@
 const express = require('express');
-
 const app = express();
-const port = 7865;
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.listen(port, () => {
+// New route: only accept numeric :id using regex
+app.get('/cart/:id(\\d+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+
+app.listen(7865, () => {
   console.log('API available on localhost port 7865');
 });
 
-module.exports = app
+module.exports = app;
